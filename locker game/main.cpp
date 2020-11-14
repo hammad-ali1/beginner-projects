53fbf8d727278e021fdcd7573f432835c844ac1f
hammad22002 <72652305+hammad22002@users.noreply.github.com>
2020-11-14 13:13:48 +0500

    Add files via upload

---
+++ locker game/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int i,n,j,l,number;
+    l=0;
+    cout<<"enter number of lockers ";
+    cin>> number;
+    number++;
+    bool locker[number];
+
+    for (n=1;n<number;++n)
+    {
+        locker[n]=0;
+
+    }
+
+    for (i=1;i<number;++i)
+    {
+
+        for(j=1;j<number;++j)
+        {
+            l=i*j;
+            if(l<number)
+         {
+             locker[l]=!locker[l];
+         }
+
+         }
+    }
+
+     for (n=1;n<number;++n)
+    {
+        if(locker[n]==1)
+        cout<<"locker no "<< n <<" is open "<<endl;
+
+    }
+    getch();
+    return 0;
+}
